281700b57525343fbee79f7701d338f4cd2046a0
AndresPope <andresfelipe-galvis@hotmail.com>
2025-05-28 20:10:51 -0500

    add - log to port

--- momento-evaluativo-3/backend/src/main.ts
+++ momento-evaluativo-3/backend/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT ?? 4000);
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log('🚀 Server ready at port:' + port);
 }
 
 bootstrap();
